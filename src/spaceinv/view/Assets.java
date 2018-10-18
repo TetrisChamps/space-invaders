0eedef05ad4d3c93dc27faa0bb03e5b7fa18ea12
David Olsson <d.e.d.davidolsson@gmail.com>
2018-10-18 14:19:16 +0200

    smurf

--- src/spaceinv/view/Assets.java
+++ src/spaceinv/view/Assets.java
@@ -29,27 +29,56 @@ public enum Assets {
     private final String IMAGE_DIR = "file:assets/img/";
     private final String SOUND_DIR = "file:assets/sound/";
 
-    private final Map<Object, Image> objectImage = new HashMap<>();
+    private Map<Object, Image> objectImage = new HashMap<>();
+
+
+
+    public void smurfMode() {
+        Map<Object, Image> smurfsObjectImage = new HashMap<>();
+
+        bind(BattleCruiser.class, getImage("battlecruiser.png"), smurfsObjectImage);
+        bind(Bomber.class, getImage("bomber.png"), smurfsObjectImage);
+        bind(Frigate.class, getImage("frigate.png"), smurfsObjectImage);
+        bind(Bomb.class, getImage("bomb.png"), smurfsObjectImage);
+        bind(Rocket.class, getImage("rocket.png"), smurfsObjectImage);
+        bind(Gun.class, getImage("cannon.png"), smurfsObjectImage);
+        bind(Ground.class, getImage("ground.png"), smurfsObjectImage);
+
+        this.objectImage = smurfsObjectImage;
+        background = getImage("smurfsBackground.png");
+        splash = getImage("smurfSplash.png");
+        explosion = getImage("explosion.png");
+    }
+
+    public void normalMode() {
+
+        Map<Object, Image> normalObjectImage = new HashMap<>();
+
+        bind(BattleCruiser.class, getImage("battlecruiser.png"), normalObjectImage);
+        bind(Bomber.class, getImage("bomber.png"), normalObjectImage);
+        bind(Frigate.class, getImage("frigate.png"), normalObjectImage);
+        bind(Bomb.class, getImage("bomb.png"), normalObjectImage);
+        bind(Rocket.class, getImage("rocket.png"), normalObjectImage);
+        bind(Gun.class, getImage("cannon.png"), normalObjectImage);
+        bind(Ground.class, getImage("ground.png"), normalObjectImage);
+
+        this.objectImage = normalObjectImage;
+        background = getImage("spaceBackground.png");
+        splash = getImage("splash.jpg");
+        explosion = getImage("explosion.png");
+
+    }
 
     // ------------ Handling Visual ------------------------
 
     public final Color colorFgText = Color.WHITE;
     public final int fontSize = 18;
 
-    public final Image background = getImage("spaceBackground.png");
-    public final Image splash = getImage("splash.jpg");
-    public final Image explosion = getImage("explosion.png");
+    public Image background ;
+    public Image splash;
+    public Image explosion;
 
     // All classes bound here (objects must be bound else where, not done in this application)
-    {
-        bind(BattleCruiser.class, getImage("battlecruiser.png"));
-        bind(Bomber.class, getImage("bomber.png"));
-        bind(Frigate.class, getImage("frigate.png"));
-        bind(Bomb.class, getImage("bomb.png"));
-        bind(Rocket.class, getImage("rocket.png"));
-        bind(Gun.class, getImage("cannon.png"));
-        bind(Ground.class, getImage("ground.png"));
-    }
 
     // -------------- Audio handling -----------------------------
 
@@ -66,7 +95,7 @@ public enum Assets {
     }
 
     // This will bind a class to an image to be used in GUI
-    public void bind(Class<?> clazz, Image image) {
+    public void bind(Class<?> clazz, Image image, Map<Object, Image> objectImage) {
         objectImage.put(clazz, image);
     }
 
